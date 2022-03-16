47a1b361fbfecb1a72ecbfc8329a1fd674415c1c
Dakota Dudley <dakotadudley13@gmail.com>
2022-03-16 17:50:38 -0400

    added requirements, virtual env, & created dicts for predicted chances and pick rate

--- BracketPicker.py
+++ BracketPicker.py
@@ -1,10 +1,8 @@
-# pip install bs4
-# pip install requests
-# pip install selenium
-# pip install webdriver_manager
 
-import requests
-import time
+# Notes: Make sure python interpreter is set to virtualenv python file
+
+import requests, time
+import array as arr
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -47,3 +45,66 @@ predChancesContent = BeautifulSoup(predictedChancesPage, "html.parser")
 
 #Use BeautifulSoup library to grab content of pickRatePage
 pickRateContent = BeautifulSoup(pickRatePage.content, "html.parser")
+
+
+#Gets rows of predicted chances table for each region
+regions = [];
+eastRegionPred = predChancesContent.find_all("table")[4].find("tbody").find_all("tr")
+midwestRegionPred = predChancesContent.find_all("table")[6].find("tbody").find_all("tr")
+southRegionPred = predChancesContent.find_all("table")[8].find("tbody").find_all("tr")
+westRegionPred = predChancesContent.find_all("table")[10].find("tbody").find_all("tr")
+regions.extend([eastRegionPred, midwestRegionPred, southRegionPred, westRegionPred])
+
+# Get rows of table with pick rates
+pickRateData = pickRateContent.find("table").find("tbody").find_all("tr")
+
+#Initialize dictionary with nested dictionary for each round to store predicted chances
+predChancesDict = { 'r1': { },
+                    'r2': { },
+                    'r3': { },
+                    'r4': { },
+                    'r5': { },
+                    'r6': { }}
+                    
+#Initialize dictionary with nested dictionary for each round to store pick rates
+pickRateDict = { 'r1': { },
+                 'r2': { },
+                 'r3': { },
+                 'r4': { },
+                 'r5': { },
+                 'r6': { }}
+
+# Add to nested dictionaries describing predicted chances for each round
+for region in regions:
+    for row in region:
+        name = row.select("tr > td")[1].get_text(strip="true")
+        for round in range(1,7):
+            teamPredChance = row.select("tr > td")[round + 1].get_text(strip="true")
+            round = 'r' + str(round)
+            predChancesDict[round][name] = float(teamPredChance.strip("%"))
+
+# Add to nested dictionaries describing pick rate for each round
+for row in pickRateData:
+    for round in range(1,7):
+        entry = row.select("tr > td")[round - 1].find_all("span")
+        name = entry[1].get_text(strip="true")
+        teamPickRate = entry[3].get_text(strip="true")
+        round = 'r' + str(round)
+        pickRateDict[round][name] = float(teamPickRate.strip("%"))
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
